321c393997a70275eaadf03f0b75988b1c542b8e
kadoka <tomiya3499@gmail.com>
2026-09-12 13:45:59 +0900

    feat: add AI action types

---
+++ Assets/KadokaShipBattler/Scripts/Runtime/AI/AiActionType.cs
@@ -0,0 +1,14 @@
+namespace KadokaShipBattler.AI
+{
+    public enum AiActionType
+    {
+        Idle = 0,
+        CarryAmmo = 1,
+        LoadCannon = 2,
+        OperateCannon = 3,
+        Repair = 4,
+        DefendShip = 5,
+        BoardEnemyShip = 6,
+        SupportAlly = 7
+    }
+}
